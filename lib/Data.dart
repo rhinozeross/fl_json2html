9622d3ad7dfb7f11ca76dd33ced55d249f0488c7
Sebastian Rasskopf <service@secrit.eu>
2021-09-13 10:08:53 +0200

    initial push
    initial commit and push to github.
    still missing: providing url to scrape from as param and url-encoding

---
+++ lib/Data.dart
@@ -0,0 +1,12 @@
+//import com.fasterxml
+
+class Data {
+  String title = "";
+  String startDate = "";
+  String endDate = "";
+  String goal = "";
+  String initiator = "";
+  String addressee= "";
+  String description = "";
+  String actualSigner = "";
+}
